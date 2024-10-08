7f0cf5d3fe571e56126b7470c86e2a114f486433
Yujin Kim <yujii22001@gmail.com>
2024-10-08 23:35:10 +0900

    Feat: Question 클래스 (subjects, content, created_date)

--- project/questions/models.py
+++ project/questions/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+  subject = models.CharField(max_length=200)
+  content = models.TextField() # 내용 길이를 예측하기 힘들 때
+  created_date = models.DateTimeField()
