81059564a3e2b4e33c026748a946adf492086dc1
Yujin Kim <yujii22001@gmail.com>
2024-10-12 01:54:21 +0900

    (w5) question 상세, 리스트 함수

--- project/questions/views.py
+++ project/questions/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from questions.models import Question
 
 # Create your views here.
-def index(request):
-  name="Kim"
-  context={'name':name} # 딕셔너리
-  return render(request,'index.html',context)
+def question_detail(request, question_id):
+  question = Question.objects.get(id=question_id)
+  context = {'question' : question} # 딕셔너리
+  return render(request,'question_detail.html',context)
+
+# 목록을 띄워주는 함수
+def question_list(request):
+  questions = Question.objects.all()
+  context = {'questions' : questions}
+  return render(request, 'question_list.html',context)
