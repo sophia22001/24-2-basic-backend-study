23e45aaf79461129d0efafcb0a90cc6fa021bebf
Yujin Kim <yujii22001@gmail.com>
2024-11-08 02:53:56 +0900

    (w9) created_date -> create_date로 수정

--- project/questions/models.py
+++ project/questions/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# models.py에 클래스를 생성하면, 장고가 클래스를 토대로 DB 테이블을 생성해준다.
 class Question(models.Model):
   subject = models.CharField(max_length=200)
   content = models.TextField() # 내용 길이를 예측하기 힘들 때
-  created_date = models.DateTimeField()
+  create_date = models.DateTimeField()
